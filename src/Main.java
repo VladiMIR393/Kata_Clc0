f85a22b097d1b89592bf7f66e4a55fc4d01f1ff9
VladiMIR393 <vlad1i3mir77@gmail.com>
2022-06-03 22:00:30 +0300

    https://github.com/VladiMIR393/Kata_Clc0.git

--- src/Main.java
+++ src/Main.java
@@ -1,60 +1,71 @@
 import java.util.Scanner;
 import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
 class Main {
-        public static void main(String[] args) {
-        Scanner pr = new Scanner(System.in);
-        String p1 = pr.nextLine();
-        String[] calc = p1.split(" ");
-        /*for (int i = 0; i < calc.length; i++)
-        {
-                System.out.println(calc[i]);
-                }   */
+        public static void main(String[] args) throws IllegalArgumentException {
+                Scanner pr = new Scanner(System.in);
 
-
+                String p1 = pr.nextLine();
+                String[] calc = p1.split(" ");
                 int a, b;
                 String c;
-
-
-                if (calc.length!=3) {
-                        System.out.println("throws Exception //т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
-                } else {
-                        if ((Integer.valueOf(calc[0]).getClass().equals(Integer.valueOf(calc[2]).getClass()))){
+                int k;
+                c = calc[1];
+                String [] arb = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+                String [] rom = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
+                if (calc.length != 3) {
+                        throw new IllegalArgumentException("// т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+                } else if (Arrays.asList(arb).contains(calc[0]) && Arrays.asList(arb).contains(calc[2])){
+                        k = 0;
+                        }else if(Arrays.asList(rom).contains(calc[0]) && Arrays.asList(rom).contains(calc[2])) {
+                        k = 1;
+                }else {
+                        throw new IllegalArgumentException("// т.к. один или оба операнда не удовлетворяют условию");
+                }
+                        try {
                                 a = Integer.valueOf(calc[0]);
-                                b = Integer.valueOf(calc[2]); 
-                        } else if () {
-                                
+                                b = Integer.valueOf(calc[2]);
+
+                        } catch (NumberFormatException e) {
+                                a = RomanToArabic.RomanToArabic(calc[0]);
+                                b = RomanToArabic.RomanToArabic(calc[2]);
                         }
-                        c = calc[1];
-                        if(c.equals("+")){
-                                System.out.println(a+b);
+                        if (c.equals("+")) {
+                                if (k == 1) {
+                                        int d = a + b;
+                                        System.out.println(ArabicToRoman.arabicToRoman(d));
+                                } else {
+                                        System.out.println(a + b);
+                                }
+                        }if (c.equals("-")) {
+                                        if ((k == 1) & (b > a)) {
+                                                throw new IllegalArgumentException("т.к. в римской системе нет отрицательных чисел");
+                                        } else if (k == 1) {
+                                                int d = a - b;
+                                                System.out.println(ArabicToRoman.arabicToRoman(d));
+                                        } else {
+                                                System.out.println(a - b);
+                                        }
+
                         }
-                        if(c.equals("-")){
-                                System.out.println(a-b);
+                        if (c.equals("*")) {
+                                        if (k == 1) {
+                                                int d = a * b;
+                                                System.out.println(ArabicToRoman.arabicToRoman(d));
+                                        } else {
+                                                System.out.println(a * b);
+                                        }
                         }
-                        if(c.equals("*")){
-                                System.out.println(a*b);
-                        }
-                        if(c.equals("/")){
-                                if(b != 0){
-                                        System.out.println(a/b);
-                                } else{
-                                        System.out.println("throws Exception // т.к. деление на 0");
+                        if (c.equals("/")) {
+                                if (k == 1) {
+                                        int d = a / b;
+                                        System.out.println(ArabicToRoman.arabicToRoman(d));
+                                } else if (b != 0) {
+                                        System.out.println(a / b);
+                                } else {
+                                        throw new IllegalArgumentException("// т.к. деление на 0");
                                 }
 
                         }
 
-                }
-                String Rm15 = "IIX";
-                int result1 = RomanToArabic.RomanToArabic(Rm15);
-                System.out.println(result1);
-
-                int ArXXVII = 27;
-                String result2 = ArabicToRoman.arabicToRoman(ArXXVII);
-                System.out.println(result2);
-
         }
-
 }
